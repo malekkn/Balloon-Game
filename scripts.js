11576db3bb92006840dd3c9679359516a890cde8
MALEK <MALEK.KANAAN@OUTLOOK.COM>
2016-11-06 12:40:34 +0100

    version 0.95

--- scripts.js
+++ scripts.js
@@ -1,26 +1,27 @@
 
 "use strict";
-
-var data = '{"kills": 0 ,"initial":"GO GO GO!!!","warn":"STOP GLITCHIN BRUH!!"}';
-
+//JSON object
+var data = '{"kills": 0 ,"initial":"GO GO GO!!!","warn":"STOP GLITCHIN BRUH!!","size":10}';
+//converting passed string to JSON Object. 
 var obj = JSON.parse(data);
 
 var num = document.getElementById("counter");
 
 num.innerHTML = obj.initial;
 
+//clone function to multiply the balloon.
 function repeater () {
   var target = document.getElementById("block");
   var cln = target.cloneNode(true);
   document.getElementById("container").appendChild(cln);
 }
- 
-for (var i = 0; i < 9 ; i ++) {
+
+for (var i = 0; i < obj.size -1 ; i ++) {
     console.log(i);
     repeater();
 
 }
-
+//counter function 
 function count() {
 	if (obj.kills < 9){
 		obj.kills+=1;
@@ -31,7 +32,7 @@ function count() {
   	 	num.innerHTML= obj.kills;
   	}
 }
-
+//start button function ---coming soon ---
 // function starting (){
 //  num.innerHTML = obj.initial;
 // }
